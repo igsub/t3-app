d071fe23f541cf8dc2f6fa96b444437d2f5c7844
Ignacio Suburu <ignacio.suburu@assertia.com.ar>
2023-04-13 20:06:43 -0300

    Update user mutation

--- src/server/api/routers/users.ts
+++ src/server/api/routers/users.ts
@@ -1,3 +1,4 @@
+import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const userRouter = createTRPCRouter({
@@ -6,5 +7,28 @@ export const userRouter = createTRPCRouter({
   }),
   getFirst: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.user.findFirst();
-  })
+  }),
+  update: publicProcedure
+    .input(
+      z.object({
+        id: z.number().min(1),
+        name: z.string().min(1),
+        email: z.string().email().min(3),
+        password: z.string().min(4)
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const user = await ctx.prisma.user.update({
+        where: {
+          id: input.id
+        },
+        data: { 
+          name: input.name,
+          email: input.email,
+          password: input.password
+        }
+      })
+
+      return user
+    })
 });
